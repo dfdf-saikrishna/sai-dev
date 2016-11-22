3b5210c824426a718cee55d180e74fa0631ee61e
Sai Krishna <Sai Krishna>
2016-11-22 19:58:06 +0530

    added workflow page

--- wp-content/plugins/erp/modules/company/views/workflow.php
+++ wp-content/plugins/erp/modules/company/views/workflow.php
@@ -1,30 +1,103 @@
+<?php  
+global $wpdb;
+$compid = $_SESSION['compid'];
+$rowpol = $wpdb->get_results("SELECT * FROM policy");
+$workflow = $wpdb->get_row("SELECT COM_Pretrv_POL_Id, COM_Posttrv_POL_Id, COM_Othertrv_POL_Id, COM_Mileage_POL_Id, COM_Utility_POL_Id FROM company WHERE COM_Id='$compid'");
+
+
+?>
 <div class="postbox">
     <div class="inside">
         <h3><?php _e( 'Company Expense Request Workflow', 'crp' ); ?></h3>
 
-        <form method="post" action="admin.php?page=Upload-Employees" enctype="multipart/form-data" id="import_form">
+        <form method="post" action="#" enctype="multipart/form-data" id="import_form">
 
             <table class="form-table">
-                <tbody>
-                    
+                <tbody id="fields_container">
                     <tr>
                         <th>
-                            <label for="type"><?php _e( 'Excel File', 'crp' ); ?></label>
+                            <label for="type"><?php _e( 'Pre Travel Request', 'crp' ); ?> <span class="required">*</span></label>
                         </th>
                         <td>
-                            <input type="file" name="csv_file" id="csv_file" />
-                            
+                            <select>
+                                <option value="volvo">-Select-</option>
+                                <?php
+                                foreach($rowpol as $value)
+				{?>
+                                <option value="<?php echo $value->POL_Id?>" <?php echo ($workflow->COM_Pretrv_POL_Id==$value->POL_Id) ? 'selected="selected"' : ''; ?> ><?php echo $value->POL_Type;?></option>
+                                 
+                                <?php } ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <label for="type"><?php _e( 'Post Travel Request', 'crp' ); ?> <span class="required">*</span></label>
+                        </th>
+                        <td>
+                            <select>
+                                <option value="volvo">-Select-</option>
+                                <?php
+                                foreach($rowpol as $value)
+				{?>
+                                <option value="<?php echo $value->POL_Id?>" <?php echo ($workflow->COM_Posttrv_POL_Id==$value->POL_Id) ? 'selected="selected"' : ''; ?> ><?php echo $value->POL_Type;?></option>
+                                 
+                                <?php } ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <label for="type"><?php _e( 'General Expense Request', 'crp' ); ?> <span class="required">*</span></label>
+                        </th>
+                        <td>
+                            <select>
+                                <option value="volvo">-Select-</option>
+                                <?php
+                                foreach($rowpol as $value)
+				{?>
+                                <option value="<?php echo $value->POL_Id?>" <?php echo ($workflow->COM_Othertrv_POL_Id==$value->POL_Id) ? 'selected="selected"' : ''; ?> ><?php echo $value->POL_Type;?></option>
+                                 
+                                <?php } ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <label for="type"><?php _e( 'Mileage Requests', 'crp' ); ?> <span class="required">*</span></label>
+                        </th>
+                        <td>
+                            <select>
+                                <option value="volvo">-Select-</option>
+                                <?php
+                                foreach($rowpol as $value)
+				{?>
+                                <option value="<?php echo $value->POL_Id?>" <?php echo ($workflow->COM_Mileage_POL_Id==$value->POL_Id) ? 'selected="selected"' : ''; ?> ><?php echo $value->POL_Type;?></option>
+                                 
+                                <?php } ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <label for="type"><?php _e( 'Utility Requests', 'crp' ); ?> <span class="required">*</span></label>
+                        </th>
+                        <td>
+                            <select>
+                                <option value="volvo">-Select-</option>
+                                <?php
+                                foreach($rowpol as $value)
+				{?>
+                                <option value="<?php echo $value->POL_Id?>" <?php echo ($workflow->COM_Utility_POL_Id==$value->POL_Id) ? 'selected="selected"' : ''; ?> ><?php echo $value->POL_Type;?></option>
+                                 
+                                <?php } ?>
+                            </select>
                         </td>
                     </tr>
                 </tbody>
-                
-
-                <tbody id="fields_container" style="display: none;">
-
-                </tbody>
             </table>
 
-            <?php submit_button( __( 'Import', 'crp' ), 'primary', 'crp_import_excel' ); ?>
+            <?php submit_button( __( 'Update', 'crp' ), 'primary', 'crp_import_excel' ); ?>
         </form>
     </div><!-- .inside -->
 </div><!-- .postbox -->
